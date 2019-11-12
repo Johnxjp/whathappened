f5dac45d9609a4077cd5ffe3a3b4c7d5dc9513dc
John Lingi <lingijohn23@gmail.com>
2019-11-12 22:42:10 +0000

    Created enum to define click location

--- google_charts.js
+++ google_charts.js
@@ -1,3 +1,10 @@
+const ClickLocationEnum = {
+  TIMEPERIOD: 1,
+  ONCHART: 2,
+  OFFCHART: 3,
+  UNDEFINED: 4
+};
+
 function extractDate() {}
 
 function searchNews(ticker, date) {
@@ -6,11 +13,37 @@ function searchNews(ticker, date) {
 
 function hasClickedChart() {}
 
+function getClickLocation(mouseEvent) {
+  // Returns an enum describing where the user clicked
+  const paths = mouseEvent.path;
+  if (paths.length === 0) {
+    return ClickLocationEnum.UNDEFINED;
+  }
+
+  // TODO: Finish
+}
+
 function sendMessage(event) {
   console.log(`Click Detected on ${window.location.href}`);
   console.log(event);
-  // chrome.runtime.sendMessage({message: "Click Event Detected"});
 }
 
-// Clicked where?
 document.body.addEventListener("click", sendMessage, true);
+
+/*
+CSS classes that I need:
+- knowledge-finance-wholepage-chart__hover-card
+- knowledge-finance-wholepage-chart__fw-uch (chart)
+- knowledge-finance-wholepage-chart__hover-card-time
+
+span vk_bk for stock ticker
+- I also need a stock ticker
+
+
+Hyperlink to further news
+a.Q2MMlc
+
+buttons
+div.qUjgX - text content
+
+*/
