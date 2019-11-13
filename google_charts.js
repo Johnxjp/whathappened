f33a666534955425b5c66ee12a0d0636b8cf4953
John Lingi <john.lingi@babylonhealth.com>
2019-11-13 10:19:42 +0000

    Extracted company and click date - TODO: Create date object - TODO: Send to background.js

--- google_charts.js
+++ google_charts.js
@@ -1,49 +1,74 @@
-const ClickLocationEnum = {
-  TIMEPERIOD: 1,
-  ONCHART: 2,
-  OFFCHART: 3,
-  UNDEFINED: 4
-};
+// TODO: Different event for mouse button down and release
+// to get the dateRange
 
-function extractDate() {}
-
-function searchNews(ticker, date) {
+function searchNews(company, dateRange) {
   // Could be in background.js? - ask question
 }
 
-function hasClickedChart() {}
-
-function getClickLocation(mouseEvent) {
-  // Returns an enum describing where the user clicked
+function hasClickedChart(mouseEvent) {
+  // Checks if clicked on chart
   const paths = mouseEvent.path;
-  if (paths.length === 0) {
-    return ClickLocationEnum.UNDEFINED;
-  }
+  const chartElementClassName = "knowledge-finance-wholepage-chart__fw-uch";
 
-  // TODO: Finish
+  for (element of paths) {
+    let elementClassName = element.className;
+    if (
+      typeof elementClassName === "string" &&
+      elementClassName.includes(chartElementClassName)
+    ) {
+      return true;
+    }
+  }
+  return false;
 }
 
-function sendMessage(event) {
+function extractCompanyName(element) {
+  // Returns the company name
+  const spanClassName = "vk_bk";
+  const spanElement = element.getElementsByClassName(spanClassName)[0];
+  return spanElement.textContent;
+}
+
+function extractDateRange(element) {
+  // Need to create a date object with (day, month, year, hour)
+  const spanClassName = "knowledge-finance-wholepage-chart__hover-card-time";
+  let dateText = element.getElementsByClassName(spanClassName)[0].textContent;
+  var [dateStart, dateEnd] = dateText.split("-");
+  if (dateEnd === undefined) {
+    dateEnd = null;
+  }
+  return [dateStart, dateEnd];
+}
+
+function financialSummaryElement() {
+  const idName = "knowledge-finance-wholepage__entity-summary";
+  return document.getElementById(idName);
+}
+
+function processClickEvent(event) {
+  // Will send a message to the background containing company and date
   console.log(`Click Detected on ${window.location.href}`);
   console.log(event);
+  if (hasClickedChart(event)) {
+    const element = financialSummaryElement();
+    console.log("Summary Element", element);
+
+    const company = extractCompanyName(element);
+    console.log("Company Name", company);
+
+    // Note dateEnd can be nukll
+    const [dateStart, dateEnd] = extractDateRange(element);
+    console.log("Date Start", dateStart, "Date End", dateEnd);
+    sendMessage(company, dateStart, dateEnd);
+  }
 }
 
-document.body.addEventListener("click", sendMessage, true);
+function sendMessage(company, dateStart, dateEnd = null) {
+  chrome.runtime.sendMessage({
+    company: company,
+    dateStart: dateStart,
+    dateEnd: dateEnd
+  });
+}
 
-/*
-CSS classes that I need:
-- knowledge-finance-wholepage-chart__hover-card
-- knowledge-finance-wholepage-chart__fw-uch (chart)
-- knowledge-finance-wholepage-chart__hover-card-time
-
-span vk_bk for stock ticker
-- I also need a stock ticker
-
-
-Hyperlink to further news
-a.Q2MMlc
-
-buttons
-div.qUjgX - text content
-
-*/
+document.body.addEventListener("click", processClickEvent, true);
