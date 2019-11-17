584823cf67e4ddfcf227f833e1c4422372430161
John Lingi <lingijohn23@gmail.com>
2019-11-17 22:26:29 +0000

    Added time parse mechanic

--- google_charts.js
+++ google_charts.js
@@ -1,6 +1,33 @@
 // TODO: Different event for mouse button down and release
 // to get the dateRange
 
+const MONTHSTOINT = {
+  jan: 0,
+  feb: 1,
+  mar: 2,
+  apr: 3,
+  may: 4,
+  jun: 5,
+  jul: 6,
+  aug: 7,
+  sept: 8,
+  oct: 9,
+  nov: 10,
+  dec: 11,
+  january: 0,
+  february: 1,
+  march: 2,
+  april: 3,
+  may: 4,
+  june: 5,
+  july: 6,
+  august: 7,
+  september: 8,
+  october: 9,
+  november: 10,
+  december: 11
+};
+
 function searchNews(company, dateRange) {
   // Could be in background.js? - ask question
 }
@@ -29,14 +56,81 @@ function extractCompanyName(element) {
   return spanElement.textContent;
 }
 
+function formatDateObject(dateText, today, timePeriod) {
+  // Bit complex because hovercard not always the same format. Depends on the
+  // age of the stock
+  switch (timePeriod) {
+    case "1 day":
+      if (isNaN(Date.parse(dateText))) {
+        let [hour, minute] = dateText.split(":");
+        return new Date(
+          today.getFullYear(),
+          today.getMonth(),
+          today.getDate(),
+          hour,
+          minute
+        );
+      } else {
+        return new Date(Date.parse(dateText));
+      }
+
+    default:
+      var date = new Date(Date.parse(dateText));
+      if (date.getFullYear() == "2001") {
+        date.setFullYear(today.getFullYear());
+      }
+  }
+}
+
+function getCurrentChartTime(chartElement) {
+  const infoText = chartElement.getElementsByTagName("g-card-section")[0]
+    .textContent;
+
+  var regex = /\d+ \w{3,4}, \d\d:\d\d/i;
+
+  var stringDate = infoText.match(regex);
+  console.log(
+    "Matched date",
+    stringDate,
+    stringDate !== null,
+    new Date(Date.parse(stringDate[0]))
+  );
+  if (stringDate !== null) {
+    stringDate = stringDate[0];
+    let today = new Date();
+    let activityDay = new Date(Date.parse(stringDate));
+    activityDay.setFullYear(today.getFullYear());
+    return activityDay;
+  }
+  return new Date();
+}
+
 function extractDateRange(element) {
   // Need to create a date object with (day, month, year, hour)
-  const spanClassName = "knowledge-finance-wholepage-chart__hover-card-time";
-  let dateText = element.getElementsByClassName(spanClassName)[0].textContent;
+  const hoverCardSpanClassName =
+    "knowledge-finance-wholepage-chart__hover-card-time";
+  const selectedTimePeriodClassName = "QiGJYb fw-ch-sel";
+  const dateText = element.getElementsByClassName(hoverCardSpanClassName)[0]
+    .textContent;
+  const selectedTimePeriod = element.getElementsByClassName(
+    selectedTimePeriodClassName
+  )[0].textContent;
+
+  const latestActivityDate = getCurrentChartTime(element);
+  console.log("Most recent market activity", latestActivityDate);
+
   var [dateStart, dateEnd] = dateText.split("-");
+  dateStart = formatDateObject(
+    dateStart,
+    latestActivityDate,
+    selectedTimePeriod
+  );
   if (dateEnd === undefined) {
     dateEnd = null;
+  } else {
+    dateEnd = formatDateObject(dateEnd, latestActivityDate, selectedTimePeriod);
   }
+
   return [dateStart, dateEnd];
 }
 
@@ -56,7 +150,7 @@ function processClickEvent(event) {
     const company = extractCompanyName(element);
     console.log("Company Name", company);
 
-    // Note dateEnd can be nukll
+    // Note dateEnd can be null
     const [dateStart, dateEnd] = extractDateRange(element);
     console.log("Date Start", dateStart, "Date End", dateEnd);
     sendMessage(company, dateStart, dateEnd);
