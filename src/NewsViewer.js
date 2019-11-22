50e32eb8a7faa4ab6cb923db833e359149deeb78
John Lingi <john.lingi@babylonhealth.com>
2019-11-22 16:11:34 +0000

    Render news in iframe

--- src/NewsViewer.js
+++ src/NewsViewer.js
@@ -6,45 +6,60 @@ class NewsViewer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      news: []
+      news: [],
+      loading: false
     };
   }
 
   componentDidMount() {
-    chrome.runtime.onMessage.addListener((request, sender, response) =>
-      this.processMessage(request, sender, response)
-    );
+    chrome.runtime.onMessage.addListener((request, sender, response) => {
+      if (request.action === "chartClicked") {
+        this.processMessage(request, sender, response);
+      }
+    });
   }
 
   processMessage(request, sender, senderResponse) {
     console.log("React: request received", request);
-    if (request.action === "chartClicked") {
-      let { company, dateStart, dateEnd } = request.data;
-      dateStart = new Date(dateStart);
-      dateEnd = dateEnd === null ? null : new Date(dateEnd);
-      getGoogleNews(company, dateStart, dateEnd)
-        .then(news => {
-          console.log(news);
-          this.setState({ news: news });
-        })
-        .catch(err => this.setState({ news: [] }));
-    }
+    this.setState({ loading: true });
+    let { company, dateStart, dateEnd } = request.data;
+    dateStart = new Date(dateStart);
+    dateEnd = dateEnd === null ? null : new Date(dateEnd);
+    getGoogleNews(company, dateStart, dateEnd)
+      .then(news => {
+        console.log(news);
+        this.setState({ news: news, loading: false });
+      })
+      .catch(err => this.setState({ news: [], loading: false }));
   }
 
   renderNews() {
-    return <h3>Rendered News</h3>;
+    if (this.state.loading) {
+      return <p>Retrieving news...</p>;
+    }
+
+    const news = this.state.news;
+    if (news.length === 0) {
+      return <p>No news</p>;
+    }
+
+    return (
+      <>
+        <p>Enjoy the news</p>
+        <ol>
+          {news.map(item => (
+            <li key={item.heading}>
+              [{item.source}: {item.time}]:{" "}
+              <a href={item.link}>{item.heading}</a>
+            </li>
+          ))}
+        </ol>
+      </>
+    );
   }
 
   render() {
-    return (
-      <div>
-        {this.state.news.length === 0 ? (
-          <h3>Nothing to show</h3>
-        ) : (
-          this.renderNews()
-        )}
-      </div>
-    );
+    return <div>{this.renderNews()}</div>;
   }
 }
 
