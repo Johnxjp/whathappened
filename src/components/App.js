6dd07dbbb82628ad2a79d3815c6c7d27edf59adf
John Lingi <lingijohn23@gmail.com>
2020-01-07 09:21:21 -0500

    Refactored components and moved icons

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,11 @@ import React from "react";
 import ViewerManager from "./ViewerManager";
 import Header from "./Header";
 
-class App extends React.Component {
-  render() {
-    return (
-      <>
-        <Header />
-        <ViewerManager />
-      </>
-    );
-  }
+export default function App() {
+  return (
+    <>
+      <Header />
+      <ViewerManager />
+    </>
+  );
 }
-
-export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -9,18 +9,16 @@ function closeWindow() {
   });
 }
 
-function Header() {
+export default function Header() {
   return (
     <div id="what-happened-header">
       <h1 id="what-happened-title">What Happened?</h1>
       <img
         id="close-button-icon"
         src="./icons/close.png"
-        alt="close_button"
+        alt="close_btn"
         onClick={closeWindow}
       />
     </div>
   );
 }
-
-export default Header;
