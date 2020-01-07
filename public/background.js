02eb2cc632d2eee39351618d24402761f3b0da53
John Lingi <lingijohn23@gmail.com>
2020-01-06 19:09:09 -0500

    Toggle icon on and off when can activate or not

--- public/background.js
+++ public/background.js
@@ -4,3 +4,19 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   console.log("I've detected a browser click");
   chrome.tabs.sendMessage(tab.id, { action: `browserActionClicked` });
 });
+
+chrome.runtime.onMessage.addListener((request, sender, response) => {
+  if (request.action === "setOnIcon") {
+    chrome.browserAction.setIcon({
+      path: "on_icon.png",
+      tabId: sender.tab.id
+    });
+  }
+
+  if (request.action === "setOffIcon") {
+    chrome.browserAction.setIcon({
+      path: "off_icon.png",
+      tabId: sender.tab.id
+    });
+  }
+});
